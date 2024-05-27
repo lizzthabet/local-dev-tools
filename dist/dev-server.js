b0498fe5f14d6c8cbd9465f19f053ca03c140481
lizz thabet <lizzthabet@gmail.com>
2024-05-26 20:57:33 -0400

    support routing to html pages without an explicit extension, plus fix a display issue with svgs

--- dist/dev-server.js
+++ dist/dev-server.js
@@ -19,32 +19,58 @@ const SERVE_CONTENT_FROM = "public";
 // Websocket server (for allowing browser and dev server to have 2-way communication)
 // We don't even need to do anything except create the instance!
 new ws_1.Server({ port: WEBSOCKET_PORT });
-// Get the file content and if it exists, inject the websocket snippet
-function getFileIfExists(route) {
-    // We don't care about performance for a dev server, so sync functions are fine.
-    // If the route exists it's either the exact file we want or the path to a directory
-    // in which case we'd serve up the 'index.html' file.
-    if ((0, fs_1.existsSync)(route)) {
+function parseFilenameIfExists(route) {
+    const noExtension = (0, path_1.extname)(route) === '';
+    const fileExists = (0, fs_1.existsSync)(route);
+    if (fileExists) {
         const fileStat = (0, fs_1.statSync)(route);
+        // If the requested file is a directory, assume there's an index page to serve
         if (fileStat.isDirectory()) {
-            return getFileIfExists((0, path_1.join)(route, 'index.html'));
+            return (0, path_1.join)(route, 'index.html');
         }
-        if (fileStat.isFile()) {
-            const file = (0, fs_1.readFileSync)(route);
-            if (route.endsWith('.html')) {
-                // Inject the client-side websocket code by adding the script to the end
-                // browsers allow for tons of deviation from *technically correct* HTML.
-                const fileWithReloadScript = `${file.toString()}\n\n<script>${CLIENT_WEBSOCKET_CODE}</script>`;
-                return Buffer.from(fileWithReloadScript);
-            }
-            return file;
+        else if (fileStat.isFile()) {
+            return route;
+        }
+    }
+    // If there's no extension and file doesn't exist, let's try parsing the route
+    // as an html file and serve it if it's present
+    if (!fileExists && noExtension) {
+        const htmlFilePath = `${route}.html`;
+        if ((0, fs_1.existsSync)(htmlFilePath)) {
+            return htmlFilePath;
         }
     }
     return null;
 }
+// Get the file content and if it exists, inject the websocket snippet
+function getFileIfExists(route) {
+    // We don't care about performance for our dev server, so sync functions are fine.
+    // If the route exists it's either the exact file we want or the path to a directory
+    // in which case we'd serve up the 'index.html' file.
+    const filepath = parseFilenameIfExists(route);
+    if (filepath) {
+        const file = (0, fs_1.readFileSync)(filepath);
+        if ((0, path_1.extname)(filepath) === ".html") {
+            // Inject the client-side websocket code by adding the script to the end;
+            // browsers allow for tons of deviation from *technically correct* HTML.
+            const fileWithReloadScript = `${file.toString()}\n\n<script>${CLIENT_WEBSOCKET_CODE}</script>`;
+            return Buffer.from(fileWithReloadScript);
+        }
+        return file;
+    }
+    return null;
+}
+// Add headers for specific media types that browsers are picky about
+function getHeadersForRoute(route) {
+    const headers = {};
+    if ((0, path_1.extname)(route) === ".svg") {
+        headers["Content-Type"] = "image/svg+xml";
+    }
+    return headers;
+}
 /** General request handler and router */
 const requestHandler = function (req, res) {
-    console.log(`request for ${req.url}`);
+    console.log(`> > request for ${req.url}`);
     const method = req.method.toLowerCase();
     if (method === 'get') {
         // No need to ensure the route can't access other local files,
@@ -52,8 +78,9 @@ const requestHandler = function (req, res) {
         const url = new URL(req.url, `http://${req.headers.host}`);
         const route = (0, path_1.normalize)((0, path_1.join)(__dirname, SERVE_CONTENT_FROM, decodeURI(url.pathname)));
         const staticFile = getFileIfExists(route);
-        if (staticFile) {
-            res.writeHead(200);
+        if (staticFile !== null) {
+            const headers = getHeadersForRoute(route);
+            res.writeHead(200, headers);
             res.end(staticFile);
             return;
         }
--- src/dev-server.ts
+++ src/dev-server.ts
@@ -1,9 +1,9 @@
 // This code and approach has been generously borrowed from:
 // https://dev.to/adamcoster/create-a-live-reload-server-for-front-end-development-3gnp
 // and tweaked with Typescript
-import { IncomingMessage, ServerResponse, createServer as createHttpServer } from 'http';
+import { IncomingMessage, OutgoingHttpHeaders, ServerResponse, createServer as createHttpServer } from 'http';
 import { readFileSync, existsSync, statSync } from 'fs';
-import { join, normalize } from 'path'
+import { join, normalize, extname } from 'path'
 import { Server as WebSocketServer } from 'ws'
 
 // Customize the port you want your server to run on
@@ -20,36 +20,65 @@ const SERVE_CONTENT_FROM = "public"
 // We don't even need to do anything except create the instance!
 new WebSocketServer({ port: WEBSOCKET_PORT });
 
-// Get the file content and if it exists, inject the websocket snippet
-function getFileIfExists(route: string): Buffer | null {
-  // We don't care about performance for a dev server, so sync functions are fine.
-  // If the route exists it's either the exact file we want or the path to a directory
-  // in which case we'd serve up the 'index.html' file.
-  if (existsSync(route)) {
+function parseFilenameIfExists(route: string): string | null {
+  const noExtension = extname(route) === ''
+  const fileExists = existsSync(route)
+  if (fileExists) {
     const fileStat = statSync(route)
+    // If the requested file is a directory, assume there's an index page to serve
     if (fileStat.isDirectory()) {
-      return getFileIfExists(join(route,'index.html'));
+      return join(route, 'index.html');
+    } else if (fileStat.isFile()) {
+      return route
     }
-
-    if (fileStat.isFile()) {
-      const file: Buffer = readFileSync(route);
-      if (route.endsWith('.html')) {
-        // Inject the client-side websocket code by adding the script to the end
-        // browsers allow for tons of deviation from *technically correct* HTML.
-        const fileWithReloadScript = `${file.toString()}\n\n<script>${CLIENT_WEBSOCKET_CODE}</script>`
-        return Buffer.from(fileWithReloadScript)
-      }
-
-      return file
+  }
+  
+  // If there's no extension and file doesn't exist, let's try parsing the route
+  // as an html file and serve it if it's present
+  if (!fileExists && noExtension) {
+    const htmlFilePath = `${route}.html`
+    if (existsSync(htmlFilePath)) {
+      return htmlFilePath
     }
   }
 
   return null
 }
 
+// Get the file content and if it exists, inject the websocket snippet
+function getFileIfExists(route: string): Buffer | null {
+  // We don't care about performance for our dev server, so sync functions are fine.
+  // If the route exists it's either the exact file we want or the path to a directory
+  // in which case we'd serve up the 'index.html' file.
+  const filepath = parseFilenameIfExists(route)
+  if (filepath) {
+    const file: Buffer = readFileSync(filepath);
+    if (extname(filepath) === ".html") {
+      // Inject the client-side websocket code by adding the script to the end;
+      // browsers allow for tons of deviation from *technically correct* HTML.
+      const fileWithReloadScript = `${file.toString()}\n\n<script>${CLIENT_WEBSOCKET_CODE}</script>`
+      return Buffer.from(fileWithReloadScript)
+    }
+
+    return file
+  }
+
+  return null
+}
+
+// Add headers for specific media types that browsers are picky about
+function getHeadersForRoute(route: string): OutgoingHttpHeaders {
+  const headers: OutgoingHttpHeaders = {}
+  if (extname(route) === ".svg") {
+    headers["Content-Type"] = "image/svg+xml"
+  }
+
+  return headers
+}
+
 /** General request handler and router */
 const requestHandler = function (req: IncomingMessage, res: ServerResponse) {
-  console.log(`request for ${req.url}`)
+  console.log(`> > request for ${req.url}`)
   const method = req.method.toLowerCase();
   if (method === 'get') {
     // No need to ensure the route can't access other local files,
@@ -57,8 +86,9 @@ const requestHandler = function (req: IncomingMessage, res: ServerResponse) {
     const url = new URL(req.url, `http://${req.headers.host}`)
     const route = normalize(join(__dirname, SERVE_CONTENT_FROM, decodeURI(url.pathname)))
     const staticFile = getFileIfExists(route)
-    if (staticFile) {
-      res.writeHead(200)
+    if (staticFile !== null) {
+      const headers = getHeadersForRoute(route)
+      res.writeHead(200, headers)
       res.end(staticFile)
       return
     }
